e0b54b220323c8f262b177fb43a2313672fb31de
Vincent Wang <vwangsf@gmail.com>
2025-04-13 00:55:11 -0400

    Initial implementation of Mehrotra's predictor-corrector algorithm with no scaling/presolve

--- solve.jl
+++ solve.jl
@@ -1,36 +1,94 @@
-#!/usr/bin/julia
+# Primal-Dual Interior-Point Solver
+# By Minh Vu, Vincent Wang
+using Printf;
+using LinearAlgebra;
 
-include("lpsolver.jl")
+include("starting_point.jl")
+include("conversions.jl")
 
-problem_list = ["lp_afiro","lp_brandy","lp_fit1d","lp_adlittle",
-"lp_agg","lp_ganges","lp_stocfor1", "lp_25fv47", "lpi_chemcom", "lp_etamacro", "lp_fffff800"]
-
-for i = 1:length(problem_list)
-    @printf("%d. %s\n", i, problem_list[i])
-end
-@printf("0. other\n")
-@printf("Which problem to solve? ")
-
-k = parse(Int,readline(stdin))
-
-if k == 0
-    @printf("Please enter the problem name (e.g. lp_afiro): ")
-    name = "LPnetlib/"*readline(stdin)[1:end-1]
-else
-    name = "LPnetlib/"*problem_list[k]
+function solve_processed(Problem, tol; maxit=100)
 end
 
-@printf("Solving %s.", name)
-
-try global P = convert_matrixdepot(mdopen(name))
-catch 
-    mdopen(name)
-    global P = convert_matrixdepot(mdopen(name))
+# argmax alpha {[0, 1] | x + alpha * dx >= 0}
+# x + alpha * dx >= 0 -> solve for alpha:
+# alpha * dx >= -x -> alpha >= -x / dx (if positive)
+# alpha <= -x / dx (if negative)
+# Note that we don't care if dx is positive since alpha is [0.0, 1.0] 
+function calcalpha(x, dx)
+    n = length(x)
+    alpha = Inf
+    for i = 1:n
+        if dx[i] < 0
+            alpha = min(alpha, -x[i] / dx[i])
+        end
+    end
+    return max(alpha, 0.0)
 end
 
-tol=1e-8
-solution = @time iplp(P, tol; maxit=200)
+function iplp(Problem, tol; maxit=100)
+    # Convert to standard form
+    A, b, c, free, bounded_below, bounded_above, bounded = tostandard(Problem)
+    m,n = size(Problem.A)
 
+    # Get initially feasible x, lambda, s
+    # TODO
+    x, lambda, s = get_starting_point(A, b, c)
 
-@show solution.flag
-@show dot(P.c,solution.x)
+    # A is 27x51 (mxn)
+    # A' is 51x27 (nxm)
+    # 51x51, 51x27, 51x51
+    # 27x51, 27x27, 27x51
+    # 51x51, 51x27, 51x51
+
+    # Implementing Mehotra's predictor-corrector algorithm (Ch. 10 of Wright)
+    for i = 1:maxit
+        # Affine direction step (10.1 Wright)
+        mat = [
+            spzeros(n, n) Problem.A' Matrix(I, n, n);
+            Problem.A spzeros(m, m) spzeros(m, n);
+            spdiagm(0 => s) spzeros(n, m) spdiagm(0 => x);
+        ]
+        rhs_aff = [
+            - (Problem.A' * lambda + s - Problem.c);
+            - (Problem.A * x - Problem.b);
+            - (x .* s)
+        ]
+        daff = mat \ rhs_aff
+        dxaff, dlambdaff, dsaff = daff[1:n], daff[n+1:n+m], daff[n+m+1:n+m+n]
+        alpha_aff_pri = min(1.0, calcalpha(x, dxaff))
+        alpha_aff_dual = min(1.0, calcalpha(s, dsaff))
+        mu = dot(x, s) / n # 1.11
+        muaff = (x + alpha_aff_pri * dxaff)' * (s + alpha_aff_dual * dsaff) / n
+        sigma = (muaff / mu)^3 # 10.3
+
+        # Centering-Corrector step from Wright 10.7
+        rhs_cc = [
+            zeros(n, 1);
+            zeros(m, 1);
+            sigma * mu .- (dxaff .* dsaff)
+        ]
+        dcc = mat \ rhs_cc
+        dxcc, dlambdacc, dscc = dcc[1:n], dcc[n+1:n+m], dcc[n+m+1:n+m+n]
+        dx, dlambda, ds = dxaff + dxcc, dlambdaff + dlambdacc, dsaff + dscc
+        alpha_pri = min(0.99 * calcalpha(x, dx), 1.0)
+        alpha_dual = min(0.99 * calcalpha(s, ds), 1.0)
+
+        if (alpha_pri > 1e308 || alpha_dual > 1e308)
+            # Very large alpha; problem is unbounded or infeasible
+            return IplpSolution(vec([]),false,vec(c),A,vec(b),vec(x),vec(lambda),vec(s))
+        end
+
+        x = x + alpha_pri * dx
+        lambda = lambda + alpha_dual * dlambda
+        s = s + alpha_dual * ds
+
+        # Check if tolerances are satisfied
+        if dot(x, s) / n <= tol && norm([A'* lambda + s - c; A * x - b; x.*s]) / norm([b;c]) <= tol
+            orig_x = fromstandard(Problem, x, free, bounded_below, bounded_above, bounded)
+            return IplpSolution(vec(orig_x),true,vec(c),A,vec(b),vec(x),vec(lambda),vec(s))
+        end
+    end
+
+    # Failed to converge in maxit iterations
+    return IplpSolution(vec([]),false,vec(c),A,vec(b),vec(x),vec(lambda),vec(s))
+end
---
+++ solve_frontend.jl
@@ -0,0 +1,21 @@
+using Printf
+
+include("problem_def.jl")
+include("solve.jl")
+
+# Get problem to be solved
+@printf("Type the name of the LPnetlib problem to be solved (e.g. lp_afiro): ")
+name = readline(stdin)
+
+# Load problem
+problem_path = "LPnetlib/"*name
+@printf("Will solve: %s\n", problem_path)
+problem = convert_matrixdepot(mdopen(problem_path))
+
+# Set params and solve problem
+tol = 1e-8
+maxit = 100
+solution = @time iplp(problem, tol; maxit)
+
+@show solution.flag
+@printf "Objective value: %lf\n" dot(problem.c,solution.x)
